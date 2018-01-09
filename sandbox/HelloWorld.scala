d5fea560c8577b9ecc2505f872c0ed2f851e2ecb
nasum <masaya.nasu@crowdworks.co.jp>
2018-01-09 16:56:35 +0900

    if文を学ぶ

--- sandbox/HelloWorld.scala
+++ sandbox/HelloWorld.scala
@@ -1,5 +1,14 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello World!!")
+
+    val age: Int = 5
+    val isSchoolStarted: Boolean = false
+
+    if (1 <= age && age <= 6 && isSchoolStarted) {
+      println("幼児です")
+    } else {
+      println("幼児ではありません")
+    }
   }
 }
